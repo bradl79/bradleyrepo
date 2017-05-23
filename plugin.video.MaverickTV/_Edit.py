8f19b33d9464cf944cbe0a76425f63f65c869997
bradley <bradl79@comcast.net>
2017-05-23 01:01:49 -0500

    update
    added new repos and addons

---
+++ plugin.video.MaverickTV/_Edit.py
@@ -0,0 +1,5 @@
+import xbmcaddon
+import base64
+
+MainBase = base64.b64decode ('aHR0cDovL3d3dy5tYXZlcmlja3JlcG8ubmV0L2RhdGEvaG9tZS9ob21lLnhtbA==')
+addon = xbmcaddon.Addon('plugin.video.MaverickTV')
